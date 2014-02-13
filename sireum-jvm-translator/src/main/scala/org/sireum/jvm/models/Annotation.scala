1c77ca4401db9f3d6aa2f7d630145b47400f348d
Robby <robby@k-state.edu>
2014-02-13 10:06:23 -0600

    Made source compatible with Scala 2.11.

--- sireum-jvm-translator/src/main/scala/org/sireum/jvm/models/Annotation.scala
+++ sireum-jvm-translator/src/main/scala/org/sireum/jvm/models/Annotation.scala
@@ -6,7 +6,7 @@ sealed abstract class AnnotationValue
 case class BracketAnnotation(val values: List[String]) extends AnnotationValue
 case class TextAnnotation(val value: String) extends AnnotationValue
 case class SimpleAnnotation(val value: String) extends AnnotationValue
-case class EmptyAnnotation extends AnnotationValue
+case class EmptyAnnotation() extends AnnotationValue
 
 class Annotation(val name: String, val value: AnnotationValue) {
   val getName = name
